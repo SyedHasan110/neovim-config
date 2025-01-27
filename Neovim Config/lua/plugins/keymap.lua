a0601debc3122010ee23b77e948f5b0392fcd244
Mohammad Hasan <70736490+SyedHasan110@users.noreply.github.com>
2025-01-27 16:54:06 +0530

    Add files via upload

---
+++ Neovim Config/lua/plugins/keymap.lua
@@ -0,0 +1,6 @@
+return {
+	vim.keymap.set("i", "<C-s>", "<Cmd>w<CR>"),
+	vim.keymap.set("n", "<C-s>", "<Cmd>w<CR>"),
+	vim.keymap.set("n", "<C-q>", "<Cmd>qa<CR>"),
+	vim.keymap.set("c", "<C-q>", "<Cmd>qa<CR>"),
+}
