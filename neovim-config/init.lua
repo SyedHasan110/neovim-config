57690ffceca6c488509569ce17524baf7c95bdec
Mohammad Hasan <70736490+SyedHasan110@users.noreply.github.com>
2025-01-31 18:19:31 +0530

    Add files via upload

--- neovim-config/init.lua
+++ neovim-config/init.lua
@@ -1,35 +1,42 @@
 vim.o.number = true
 require("config.lazy")
 for _, diag in ipairs({ "Error", "Warn", "Info", "Hint" }) do
-	vim.fn.sign_define("DiagnosticSign" .. diag, {
-		text = "",
-		texthl = "DiagnosticSign" .. diag,
-		linehl = "",
-		numhl = "DiagnosticSign" .. diag,
-	})
+    vim.fn.sign_define("DiagnosticSign" .. diag, {
+        text = "",
+        texthl = "DiagnosticSign" .. diag,
+        linehl = "",
+        numhl = "DiagnosticSign" .. diag,
+    })
 end
 vim.diagnostic.config({
-	virtual_text = {
-		prefix = "●", -- Could be '●', '▎', 'x'
-	},
-	update_in_insert = true,
-	severity_sort = true,
+    virtual_text = {
+        prefix = "●", -- Could be '●', '▎', 'x'
+    },
+    update_in_insert = true,
+    severity_sort = true,
 })
 
 
 vim.api.nvim_create_augroup("lualine_augroup", { clear = true })
 vim.api.nvim_create_autocmd("User", {
-	group = "lualine_augroup",
-	pattern = "LspProgressStatusUpdated",
-	callback = require("lualine").refresh,
+    group = "lualine_augroup",
+    pattern = "LspProgressStatusUpdated",
+    callback = require("lualine").refresh,
 })
 vim.opt.list = true
 vim.opt.listchars = {
-	space = '‧', -- Show spaces as middle dots (·)
-	tab = '‧‧', -- Show tabs as arrows followed by a space (→)
-	trail = '‧', -- Show trailing spaces as dots (·)
-	nbsp = '‧',
+    space = '‧', -- Show spaces as middle dots (·)
+    tab = '‧‧', -- Show tabs as arrows followed by a space (→)
+    trail = '‧', -- Show trailing spaces as dots (·)
+    nbsp = '‧',
 }
+
+vim.opt.tabstop = 4        -- 4 spaces for tabs (prettier default)
+vim.opt.softtabstop = 4    -- 4 spaces for tabs (prettier default)
+vim.opt.shiftwidth = 4     -- 4 spaces for indent width
+vim.opt.autoindent = true  -- copy indent from current line when starting new one
+vim.opt.expandtab = true   -- expand tabs
+vim.opt.smartindent = true -- indents smart
 
 vim.api.nvim_set_hl(0, "Visual", { link = "Visual", bg = "#3a3f58", })
 vim.api.nvim_set_hl(0, "Whitespace", { fg = "#6d7999", })
@@ -37,13 +44,13 @@ vim.api.nvim_set_hl(0, "Whitespace", { fg = "#6d7999", })
 vim.opt.wrap = true
 local ft = require("terminal")
 ft.setup({
-	shell = "bash", -- Change default shell
-	border = "single", -- Border style
-	width = 0.65,  -- Relative width
-	height = 0.7,  -- Relative height
-	title_pos = "center", -- Title alignment
-	esc_close = true, -- Disable Esc closing
-	title = "  Terminal ",
+    shell = "bash",       -- Change default shell
+    border = "single",    -- Border style
+    width = 0.65,         -- Relative width
+    height = 0.7,         -- Relative height
+    title_pos = "center", -- Title alignment
+    esc_close = true,     -- Disable Esc closing
+    title = "  Terminal ",
 
 })
 require("keymap")
--- neovim-config/lua/plugins/completion.lua
+++ neovim-config/lua/plugins/completion.lua
@@ -1,113 +1,113 @@
 return {
-	{
-		'saghen/blink.cmp',
-		-- optional: provides snippets for the snippet source
-		dependencies = 'rafamadriz/friendly-snippets',
+    {
+        'saghen/blink.cmp',
+        -- optional: provides snippets for the snippet source
+        dependencies = 'rafamadriz/friendly-snippets',
 
-		-- use a release tag to download pre-built binaries
-		version = '*',
-		-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-		-- build = 'cargo build --release',
-		-- If you use nix, you can build from source using latest nightly rust with:
-		-- build = 'nix run .#build-plugin',
+        -- use a release tag to download pre-built binaries
+        version = '*',
+        -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+        -- build = 'cargo build --release',
+        -- If you use nix, you can build from source using latest nightly rust with:
+        -- build = 'nix run .#build-plugin',
 
-		---@module 'blink.cmp'
-		---@type blink.cmp.Config
-		opts = {
-			signature = {
-				enabled = true,
-				window = {
-					border = "single",
-					treesitter_highlighting = true
-				}
-			},
-			completion = {
-				menu = {
-					min_width = 30,
-					max_height = 20,
-					border = 'rounded',
-					winblend = 0,
-					scrolloff = 0,
-					draw = {
-						gap = 2,
-						padding = 2,
-						treesitter = { 'lsp' },
-					}
-				},
-				documentation = {
-					auto_show = true,
-					auto_show_delay_ms = 100,
-					window = {
-						min_width = 40,
-						max_width = 50,
-						max_height = 40,
-						border = 'rounded',
-						scrollbar = true,
-						winblend = 0,
-					},
-				}
+        ---@module 'blink.cmp'
+        ---@type blink.cmp.Config
+        opts = {
+            signature = {
+                enabled = true,
+                window = {
+                    border = "single",
+                    treesitter_highlighting = true
+                }
+            },
+            completion = {
+                menu = {
+                    min_width = 30,
+                    max_height = 20,
+                    border = 'rounded',
+                    winblend = 0,
+                    scrolloff = 0,
+                    draw = {
+                        gap = 2,
+                        padding = 2,
+                        treesitter = { 'lsp' },
+                    }
+                },
+                documentation = {
+                    auto_show = true,
+                    auto_show_delay_ms = 100,
+                    window = {
+                        min_width = 40,
+                        max_width = 50,
+                        max_height = 40,
+                        border = 'rounded',
+                        scrollbar = true,
+                        winblend = 0,
+                    },
+                }
 
-			},
-			-- 'default' for mappings similar to built-in completion
-			-- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-			-- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-			-- See the full "keymap" documentation for information on defining your own keymap.
-			keymap = { preset = 'enter', cmdline = { preset = "default" } },
+            },
+            -- 'default' for mappings similar to built-in completion
+            -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+            -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+            -- See the full "keymap" documentation for information on defining your own keymap.
+            keymap = { preset = 'enter', cmdline = { preset = "default" } },
 
-			appearance = {
-				highlight_ns = vim.api.nvim_create_namespace('blink_cmp'),
-				-- Sets the fallback highlight groups to nvim-cmp's highlight groups
-				-- Useful for when your theme doesn't support blink.cmp
-				-- Will be removed in a future release
-				use_nvim_cmp_as_default = true,
-				-- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-				-- Adjusts spacing to ensure icons are aligned
-				nerd_font_variant = 'mono',
-				kind_icons = {
-					Text = '󰦨',
-					Method = '',
-					Function = '',
-					Constructor = '',
-					Field = '',
-					Variable = '',
-					Property = '',
+            appearance = {
+                highlight_ns = vim.api.nvim_create_namespace('blink_cmp'),
+                -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+                -- Useful for when your theme doesn't support blink.cmp
+                -- Will be removed in a future release
+                use_nvim_cmp_as_default = true,
+                -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+                -- Adjusts spacing to ensure icons are aligned
+                nerd_font_variant = 'mono',
+                kind_icons = {
+                    Text = '󰦨',
+                    Method = '',
+                    Function = '',
+                    Constructor = '',
+                    Field = '',
+                    Variable = '',
+                    Property = '',
 
-					Class = '',
-					Interface = '',
-					Struct = '',
-					Module = '',
+                    Class = '',
+                    Interface = '',
+                    Struct = '',
+                    Module = '',
 
-					Unit = '',
-					Value = '󰦨',
-					Enum = '',
-					EnumMember = '',
+                    Unit = '',
+                    Value = '󰦨',
+                    Enum = '',
+                    EnumMember = '',
 
-					Keyword = '',
-					Constant = '',
+                    Keyword = '',
+                    Constant = '',
 
-					Snippet = '',
-					Color = '',
-					File = '',
-					Reference = '',
-					Folder = '',
-					Event = '',
-					Operator = '',
-					TypeParameter = '󰰤',
-				},
-			},
+                    Snippet = '',
+                    Color = '',
+                    File = '',
+                    Reference = '',
+                    Folder = '',
+                    Event = '',
+                    Operator = '',
+                    TypeParameter = '󰰤',
+                },
+            },
 
-			-- Default list of enabled providers defined so that you can extend it
-			-- elsewhere in your config, without redefining it, due to `opts_extend`
-			sources = {
-				default = { 'lsp', 'path', 'snippets', 'buffer' },
-			},
-		},
-		opts_extend = { "sources.default" }
-	},
-	-- LSP servers and clients communicate which features they support through "capabilities".
-	--  By default, Neovim supports a subset of the LSP specification.
-	--  With blink.cmp, Neovim has *more* capabilities which are communicated to the LSP servers.
-	--  Explanation from TJ: https://youtu.be/m8C0Cq9Uv9o?t=1275
-	--
-	-- This can vary by config, but in general for nvim-lspconfig:
+            -- Default list of enabled providers defined so that you can extend it
+            -- elsewhere in your config, without redefining it, due to `opts_extend`
+            sources = {
+                default = { 'lsp', 'path', 'snippets', 'buffer' },
+            },
+        },
+        opts_extend = { "sources.default" }
+    },
+    -- LSP servers and clients communicate which features they support through "capabilities".
+    --  By default, Neovim supports a subset of the LSP specification.
+    --  With blink.cmp, Neovim has *more* capabilities which are communicated to the LSP servers.
+    --  Explanation from TJ: https://youtu.be/m8C0Cq9Uv9o?t=1275
+    --
+    -- This can vary by config, but in general for nvim-lspconfig:
 }
