b65ef19001acb614e15caa62e456f71372f8625f
Mohammad Hasan <70736490+SyedHasan110@users.noreply.github.com>
2025-01-31 15:21:10 +0530

    Delete Neovim Config directory

--- Neovim Config/lua/plugins/keymap.lua
+++
@@ -1,6 +0,0 @@
-return {
-	vim.keymap.set("i", "<C-s>", "<Cmd>w<CR>"),
-	vim.keymap.set("n", "<C-s>", "<Cmd>w<CR>"),
-	vim.keymap.set("n", "<C-q>", "<Cmd>qa<CR>"),
-	vim.keymap.set("c", "<C-q>", "<Cmd>qa<CR>"),
-}
