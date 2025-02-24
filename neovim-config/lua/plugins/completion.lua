97a18f94bfab1770517bce075df48223516cd118
Mohammad Hasan <70736490+SyedHasan110@users.noreply.github.com>
2025-02-24 20:53:39 +0530

    Add files via upload

--- neovim-config/lua/plugins/completion.lua
+++ neovim-config/lua/plugins/completion.lua
@@ -24,7 +24,7 @@ return {
                 window = {
                     border = "single",
                     treesitter_highlighting = true,
-                    show_documentation = false
+                    show_documentation = true
                 }
             },
             keymap = {
@@ -80,7 +80,7 @@ return {
                 -- Sets the fallback highlight groups to nvim-cmp's highlight groups
                 -- Useful for when your theme doesn't support blink.cmp
                 -- Will be removed in a future release
-                use_nvim_cmp_as_default = true,
+                use_nvim_cmp_as_default = false,
                 -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
                 -- Adjusts spacing to ensure icons are aligned
                 nerd_font_variant = 'normal',
